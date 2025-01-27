1e739dda2084ddd12266d7aecdbadf84136d9d06
Leslie Kogi <lesliekogi@gmail.com>
2025-01-27 20:47:07 +0300

    Js initial stages

---
+++ scr/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("akanform").addEventListener("submit",function(event){
+        event.preventDefault()
+        usersinput()
+    })
+})
+function usersinput(){
+    const birthdate=document.getElementsByClassName("").value;
+    const gender=document.getElementById("").value;
+}
