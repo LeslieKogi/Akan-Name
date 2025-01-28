ba798cb25bc1424505c1f5b91f7b4bf8e75a495a
Leslie Kogi <lesliekogi@gmail.com>
2025-01-28 15:53:30 +0300

    js modifications b4 adding formulae

--- scr/main.js
+++ scr/main.js
@@ -5,6 +5,28 @@ document.addEventListener("DOMContentLoaded",function(){
     })
 })
 function usersinput(){
-    const birthdate=document.getElementsByClassName("").value;
-    const gender=document.getElementById("").value;
+    const birthdate = document.getElementById("birthdate").value;
+    const gender= document.getElementById("genderselect").value;
+    
+    console.log(birthdate,gender)
+    if(!birthdate||!gender){
+        alert("Please fill out all details")
+        return
+    }
+    //array destructure 2025-01-30
+    const[year,month,day]=birthdate.split("-").map(Number)
+    console.log(year,month,day)
+    
+    
+    //validating
+    if(day<=0 || day>31 || month<=0 || month>12){
+        alert("invalid birthdate given")
+        return
+
+}
+
+determineAkanName(year,month,day,gender)
+}
+function determineAkanName(year,month,day,gender){
+console.log("From the determineAkanName function",year,month,day,gender)
 }
